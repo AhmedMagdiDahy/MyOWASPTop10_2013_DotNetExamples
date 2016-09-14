76268f9f9d17c7bd0739cd151c9286ca71f19a2f
djtcarrilho <djtcarrilho@hotmail.com>
2016-09-14 12:42:10 +0100

    refactor calendar hack example both clan code and sercure server ide

--- ExampleCalendarControlHacked/Default.aspx.cs
+++ ExampleCalendarControlHacked/Default.aspx.cs
@@ -32,7 +32,7 @@ public partial class _Default : System.Web.UI.Page
 
         if (e.Day.Date == DateTime.Today)
         {
-            if (isValidTimeOfDayToScheduleMeal())
+            if (isValidTimeOfDayToScheduleMeal_())
             {
                 e.Day.IsSelectable = false;
                 e.Cell.ForeColor = System.Drawing.Color.Red;
@@ -47,44 +47,64 @@ public partial class _Default : System.Web.UI.Page
         }
     }
 
-    protected void Calendar1_DayRender(object sender, DayRenderEventArgs e)
-    {
-        if (DateTime.Compare(e.Day.Date, System.DateTime.Today) < 0)
-        {
-            e.Day.IsSelectable = false;
-            e.Cell.ForeColor = System.Drawing.Color.Red;
-        }
-
-
-        if (e.Day.Date == DateTime.Today)
-        {
-            if (isValidTimeOfDayToScheduleMeal())
-            {
-                e.Day.IsSelectable = false;
-                e.Cell.ForeColor = System.Drawing.Color.Red;
-                e.Cell.BackColor = ColorTranslator.FromHtml("#F4F4F4");
-                if (!Page.IsPostBack)
-                {
-                    Calendar1.SelectedDate = DateTime.Today.AddDays(1);
-                }
-            }
-        }
-    }
-
-    private bool isValidTimeOfDayToScheduleMeal()
+    private bool isValidTimeOfDayToScheduleMeal_()
     {
         //refactor magic number
         //return DateTime.Now.TimeOfDay > DateTime.Today.AddHours(10).TimeOfDay;
         return DateTime.Now.TimeOfDay > DateTime.Today.AddHours(valideTimeInHoursToScheduleMeal).TimeOfDay;
     }
 
+    protected void Calendar1_DayRender(object sender, DayRenderEventArgs e)
+    {
+        //DateTime renderedDate = e.Day.Date;
+
+        //DateTime maxDateHourToSchedule = DateTime.Today.AddHours(10);
+        //bool sameDay = renderedDate.Date == maxDateHourToSchedule.Date;
+
+        //bool isValidTimeOfDayToScheduleMeal = (sameDay && DateTime.Now > maxDateHourToSchedule )  || renderedDate.Date < maxDateHourToSchedule.Date ;
+
+        if (isInValidTimeOfDayToScheduleMeal(e.Day.Date ))
+        {
+            e.Day.IsSelectable = false;
+            e.Cell.ForeColor = System.Drawing.Color.Red;       
+        }
+
+        //if (sameDay) // refactoring the previous code to a new method removed access to this computed value...
+        if (e.Day.Date == DateTime.Now.Date )
+        {
+            e.Cell.BackColor = Color.Cyan;
+        }
+    }
+
+
+    private bool isInValidTimeOfDayToScheduleMeal(DateTime date)
+    {
+        //refactor magic number
+
+        DateTime maxDateHourToSchedule = DateTime.Today.AddHours(valideTimeInHoursToScheduleMeal);
+        bool sameDay = date.Date == maxDateHourToSchedule.Date;
+
+        //refactor do not need this extra variable
+        //bool isValidTimeOfDayToScheduleMeal = (sameDay && DateTime.Now > maxDateHourToSchedule) || renderedDate.Date < maxDateHourToSchedule.Date;
+        //return isValidTimeOfDayToScheduleMeal;
+
+        return (sameDay && DateTime.Now > maxDateHourToSchedule) || date.Date < maxDateHourToSchedule.Date;
+    }
+
     protected void Calendar1_SelectionChanged(object sender, EventArgs e)
     {
         string dataEscolhida = Calendar1.SelectedDate.ToShortDateString();
-        string scriptInfo = "alert('" + "ok para dia: " + dataEscolhida + "');";
-
+        string scriptInfo = "";
         //VERIFICAR NOVAMENTE A VALIDADE DA DATA!!
+        if (!isInValidTimeOfDayToScheduleMeal(Calendar1.SelectedDate)){
 
+            scriptInfo = "alert('" + "ok para dia: " + dataEscolhida + "');";
+        }
+
+        else{
+
+            scriptInfo = "alert('" + "Não pode selecionar a data: " + dataEscolhida + "');";
+        }
         ClientScript.RegisterStartupScript(this.GetType(), "infoOK", scriptInfo, true);
     }
 }
