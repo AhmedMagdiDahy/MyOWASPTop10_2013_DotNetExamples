12732bf68235226cd1d104bc3af6f87adc23fb37
djtcarrilho <djtcarrilho@hotmail.com>
2016-09-14 12:38:42 +0100

    clean code 1 and git ignore

--- ExampleCalendarControlHacked/Default.aspx.cs
+++ ExampleCalendarControlHacked/Default.aspx.cs
@@ -8,13 +8,45 @@ using System.Drawing;
 
 public partial class _Default : System.Web.UI.Page
 {
-    private const int V = 10;
+    private const int valideTimeInHoursToScheduleMeal = 10;
 
     protected void Page_Load(object sender, EventArgs e)
     {
-        
+
 
     }
+
+    /// <summary>
+    /// good example that needs refactoring
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void Calendar1_DayRender_(object sender, DayRenderEventArgs e)
+    {
+        if (DateTime.Compare(e.Day.Date, System.DateTime.Today) < 0)
+        {
+            e.Day.IsSelectable = false;
+            e.Cell.ForeColor = System.Drawing.Color.Red;
+        }
+
+
+        if (e.Day.Date == DateTime.Today)
+        {
+            if (isValidTimeOfDayToScheduleMeal())
+            {
+                e.Day.IsSelectable = false;
+                e.Cell.ForeColor = System.Drawing.Color.Red;
+                e.Cell.BackColor = ColorTranslator.FromHtml("#F4F4F4");
+                if (!Page.IsPostBack)
+                {
+                    Calendar1.SelectedDate = DateTime.Today.AddDays(1);
+                }
+            }
+
+
+        }
+    }
+
     protected void Calendar1_DayRender(object sender, DayRenderEventArgs e)
     {
         if (DateTime.Compare(e.Day.Date, System.DateTime.Today) < 0)
@@ -34,12 +66,8 @@ public partial class _Default : System.Web.UI.Page
                 if (!Page.IsPostBack)
                 {
                     Calendar1.SelectedDate = DateTime.Today.AddDays(1);
-
-                   // Calendar1_SelectionChanged(sender, null);
                 }
             }
-
-
         }
     }
 
@@ -47,7 +75,7 @@ public partial class _Default : System.Web.UI.Page
     {
         //refactor magic number
         //return DateTime.Now.TimeOfDay > DateTime.Today.AddHours(10).TimeOfDay;
-        return DateTime.Now.TimeOfDay > DateTime.Today.AddHours(V).TimeOfDay;
+        return DateTime.Now.TimeOfDay > DateTime.Today.AddHours(valideTimeInHoursToScheduleMeal).TimeOfDay;
     }
 
     protected void Calendar1_SelectionChanged(object sender, EventArgs e)
