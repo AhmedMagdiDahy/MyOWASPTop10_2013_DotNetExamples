54c077f2b87c24a85c2d308d060b97e2cf10596f
djtcarrilho <djtcarrilho@hotmail.com>
2016-09-14 14:03:23 +0100

    code base for A0 Validation Examples

---
+++ My_OWASP_Top10_2013_Examples/OWASP_A0_ValidationExamples/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OWASP_A0_ValidationExamples.MvcApplication" Language="C#" %>
---
+++ My_OWASP_Top10_2013_Examples/OWASP_A0_ValidationExamples/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OWASP_A0_ValidationExamples.Startup))]
+namespace OWASP_A0_ValidationExamples
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
